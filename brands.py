a4067f034b1953d750273122a0beffa1d09d8740
matixmercury@gmail.com <matixmercury@gmail.com>
2022-06-08 16:57:18 +0430

    function created html file saved

--- brands.py
+++ brands.py
@@ -1,12 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
 
+main_brand_list = list()
+main_brands = list()
+
 req_brands = requests.get('https://www.crucial.com/upgrades')
+soup_brands = BeautifulSoup(req_brands.content, "lxml")
 
-soup_brands = BeautifulSoup(req_brands.content, "html.parser")
 
-brand_links = soup_brands.find_all("a", {"class": "small text-left button hollow secondary element_item"})
+def extract_main(soup):
 
-for iterator in brand_links:
-    print(iterator.text)
-    print('https://www.crucial.com' + iterator["href"])
+    with open(f'./main.html', 'w') as writer:
+
+        writer.write(str(soup))
+
+    with open(f'./main.html', 'r') as reader:
+
+        brand_links = reader.read()
+        inspector = BeautifulSoup(brand_links, 'lxml')
+        tag = inspector.find_all("a", {"class": "small text-left button hollow secondary element_item"})
+
+    for iterator in tag:
+        main_brand_list.append('https://www.crucial.com' + iterator["href"])
+        main_brands.append(iterator.text)
+
+
+if __name__ == "__main__":
+    extract_main(soup_brands)
+    print(main_brands)
+    print(main_brand_list)
