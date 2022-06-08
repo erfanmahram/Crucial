615db9dc30227b104efb40818099fadbafaa0651
Erfan Mahram <erfanmahram@gmail.com>
2022-06-08 16:05:34 +0430

    reading html files

--- testzz.py
+++ testzz.py
@@ -4,5 +4,9 @@ from bs4 import BeautifulSoup
 
 
 def test_suggestion_extract():
-    path = pathlib.Path.cwd().joinpath('/test')
+    path = pathlib.Path.cwd().joinpath('test')
+    for pth in path.glob('model_*.html'):
+        soup = BeautifulSoup(pth.read_text(), 'lxml')
+        result = suggestion_extract(soup)
+
 
