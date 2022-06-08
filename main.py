1583e4fdcc84b32f5cd9f352c3f480613d6c9fd4
Erfan Mahram <erfanmahram@gmail.com>
2022-06-08 20:06:19 +0430

    encoding jsons

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import re
+import json
 
 
 def suggestion_extract(soup):
-    pass
+    pattern = re.compile(r"var prodListJS(.*?)';$", re.MULTILINE | re.DOTALL)
+    rawJ = soup.find_all('script', attrs={'type': 'text/javascript'}, text=pattern)
+    pattern2 = r"var prodListJS(memory|ssd|Externalssd) = '(.*?)';$"
+    ad = re.compile(r'\xe2\x80\xa2')
+    result = dict()
+    for item in rawJ:
+        json_file = re.findall(pattern2, item.text, re.MULTILINE)
+        assert len(json_file) == 1
+        js = json.loads(json_file[0][1].encode('utf8').decode('unicode_escape'))
+        for a in js:
+            if 'specs' in a:
+                a['specs'] = [f.strip() for f in ad.split(a['specs'])]
+            # if a == 'specs':
+            #     [f.strip() for f in ad.split(a)]
+        result[json_file[0][0]] = js
+    return result
 
--- testzz.py
+++ testzz.py
@@ -6,7 +6,8 @@ from bs4 import BeautifulSoup
 def test_suggestion_extract():
     path = pathlib.Path.cwd().joinpath('test')
     for pth in path.glob('model_*.html'):
-        soup = BeautifulSoup(pth.read_text(), 'lxml')
+        soup = BeautifulSoup(pth.read_text(encoding='utf-8'), 'lxml')
         result = suggestion_extract(soup)
 
+        print(result)
 
