32ce2bc9cdb77902bc3cfa5038e80c1ad18ed7ba
Najme <51949100+CodeTild@users.noreply.github.com>
2022-06-25 00:09:25 -0700

    Update models.py
    Without on-to-one....relations

--- models.py
+++ models.py
@@ -7,23 +7,19 @@ class Student(models.Model):
     family_name = models.CharField(max_length=100)
     gpa = models.FloatField(null=True)
     def __str__(self):
-        return self.first_name#,self.family_name , self.gpa
+        return self.first_name
      
 class Professor(models.Model):
     name = models.CharField(max_length=100)
     pass_percent = models.FloatField(null=True)
-    #student= models.ManyToManyField(Student)
 
 class Course(models.Model):
     course_name = models.CharField(max_length=100)
-    prof_name = models.CharField(max_length=100)
-    #professor=models.ManyToManyField(Professor)
-    #student= models.ManyToManyField(Student)
-    
+    p_ID = models.IntegerField(null=True)
+        
 class Exam(models.Model):
-   #course = models.OneToOneField(Course,on_delete=models.CASCADE,
-   #    primary_key=True)
     s_ID = models.IntegerField()
     c_ID = models.IntegerField()
     grade = models.FloatField()
     
+
