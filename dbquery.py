534924b5e8c2e242a5e164bcbe4a92a2e9dee6a5
Najme <51949100+CodeTild@users.noreply.github.com>
2022-06-25 00:14:18 -0700

    Update dbquery.py
    Updated commands  in CMD for accessing db

--- dbquery.py
+++ dbquery.py
@@ -1,6 +1,11 @@
+# -*- coding: utf-8 -*-
+#os level command
+#Scripts\activate.bat
+# cd path
 python manage.py makemigrations
 python manage.py migrate 
-
+python
+>>>
 import os;
 os.environ['DJANGO_SETTINGS_MODULE'] = 'project1.settings';
 import django;
@@ -9,89 +14,86 @@ django.setup();
 from app1.models import Student;  
 from app1.models import Professor; 
 from app1.models import Course;
-from app1.models import Exam; 
-    
-s1 =Student(first_name = "AA", family_name = "BB")
-s1.save()
-s2 =Student(first_name = "CC", family_name = "DD")
-s2.save()
-s3 =Student(first_name = "EE", family_name = "FF")
-s3.save()
- 
-#p1.student.add(s1,s2,s3)
-#p2.student.add(s1,s2)
-#p3.student.add(s2,s3)  
-    
-p1 = Professor(name ="profAA")
-p1.save()
-p2 = Professor(name ="profBB")
-p2.save()
-p3 = Professor(name ="profCC")
-p3.save()
+from app1.models import Exam;
+from django.db.models import Count
+from django.db.models import Q 
 
-c1= Course(course_name="course1", prof_name="profBB") 
-c1.save()
-c2= Course(course_name="course2", prof_name="profAA") 
-c2.save()
-c3= Course(course_name="course3", prof_name="profCC") 
-c3.save()
+import string 
+import random
+
+#Filling Student table
+for i in range (0,200):
+    string_name = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
+    string_family = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
+    s =Student(first_name = string_name, family_name = string_family)
+    s.save()
+#Filling Professor table
+for i in range (0,10):
+    string_name = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
+    string_family = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
+    p= Professor(name = string_name+" "+ string_family)
+    p.save()
+#Filling Course table
+##each professor have two courses.
+for i in range (1,21):
+    name = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
+    if (i>10):
+        c= Course(course_name=name, p_ID = i-10)
+    else:
+        c= Course(course_name=name, p_ID = i)
+        
+    c.save()
     
-e1= Exam(s_ID=1,c_ID=2,grade=12.5)
-e1.save()
-e2= Exam(s_ID=1,c_ID=3,grade=17)
-e2.save()
+
     
-e3= Exam(s_ID=2,c_ID=1,grade=17)
-e3.save()   
-
-e4= Exam(s_ID=2,c_ID=2,grade=19)
-e4.save()   
-
-e5= Exam(s_ID=2,c_ID=3,grade=5)
-e5.save()   
-
-e6= Exam(s_ID=3,c_ID=1,grade=9)
-e6.save()
-   
-e7= Exam(s_ID=3,c_ID=3,grade=6)
-e7.save()   
+#Filling Exam
+#Each student can have up to max_num courses
+max_num=4
+s_num =  Student.objects.count()
+c_num =  Course.objects.count()
+course_num_list=[random.randint(1, max_num) for _ in range(s_num)]
+#e_num=sum(course_num_list)
+sid=1
+for i in range (0, s_num ):
+    a=course_num_list[i]
+    for j in range(0,a):
+        e=Exam (s_ID = sid, c_ID=random.randint(1,c_num), grade=round(random.uniform(0, 20), 2) )
+        e.save()
+        if j== a-1:
+            sid=sid+1
 
 #Calculating  GPA for each student 
 from django.db.models import Avg
-for i in range(1,4):
+for i in range(1,s_num+1):
     x = Exam.objects.filter (s_ID=i)
     item=Student.objects.get(id=i)
     avg=x.all().aggregate(Avg('grade'))
-    item.gpa=avg['grade__avg']
+    item.gpa=round(avg['grade__avg'],2)
     item.save()
 
 for x in Student.objects.all():
-    print(x.first_name)
-    print(x.family_name)
-    print(x.gpa)
+    print(x.first_name,' ',x.family_name,' ', x.gpa )
     
-#Calculating pass percent for each profecessor
-from django.db.models import Count
-from django.db.models import Q
-for x in Professor.objects.all():
-    y=x.name;
-    prof_course= Course.objects.get (prof_name=y)
-    course_id =prof_course.id
-    total = Exam.objects.filter (c_ID=course_id).all().count()
-    student_paased = Exam.objects.filter (Q(c_ID=course_id) & Q(grade__gte=10)).all().count()
-    x.pass_percent=student_paased/total*100
-    x.save()
     
-for x in Professor.objects.all():
-    print(x.name)
-    print(x.pass_percent)
+#Calculating pass percent for each professor
+#Eech professor has two courses. Pass_percent is calculated based on two courses 
 
-for y in Course.objects.all():
-    print(y.course_name)
-    print(y.prof_name)
+for p in Professor.objects.all():
+    p_courses= Course.objects.filter (p_ID=p.id)#return queryset
+    total =0
+    student_paased=0
+    for c in p_courses.all():
+        total = total + Exam.objects.filter (c_ID=c.id).all().count()
+        student_paased = student_paased + Exam.objects.filter (Q(c_ID=c.id) & Q(grade__gte=10)).all().count()
+    p.pass_percent=round(student_paased/total*100,2)
+    p.save()
     
+for p in Professor.objects.all():
+    print(p.name, " ",p.pass_percent )
     
-    
-      
-    
-   
+for c in Course.objects.all():
+    print(c.course_name," ", c.p_ID)
+
+for e in Exam.objects.all():
+    print(e.s_ID," ", e.c_ID, " " ,e.grade)
+  
