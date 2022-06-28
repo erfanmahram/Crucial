a717922d6ada4f272b95fac83b85a39204c2de03
Najme <51949100+CodeTild@users.noreply.github.com>
2022-06-28 04:26:22 -0700

    Update dbquery.py
    Calculating pass percent for each professor will be added based on the correction results of this version

--- dbquery.py
+++ dbquery.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 26 01:57:18 2022
+
+@author: PC
+"""
+# -*- coding: utf-8 -*-
 #os level command
 #Scripts\activate.bat
 # cd path
-python manage.py makemigrations
-python manage.py migrate 
-python
->>>
+#python manage.py makemigrations
+#python manage.py migrate 
+#python
+#>>>
+
 import os;
 os.environ['DJANGO_SETTINGS_MODULE'] = 'project1.settings';
+os.environ['DJANGO_ALLOW_ASYNC_UNSAFE'] =  "true"; # It is needed in an IDE 'Spyder'
 import django;
 django.setup();
 
@@ -15,85 +23,114 @@ from app1.models import Student;
 from app1.models import Professor; 
 from app1.models import Course;
 from app1.models import Exam;
+
 from django.db.models import Count
-from django.db.models import Q 
+
 
 import string 
 import random
 
+Exam.objects.all().delete()
+Student.objects.all().delete()
+Professor.objects.all().delete()
+Course.objects.all().delete()
+
+# Assumptions:
+s_num = 200 # number of students 
+p_num =  10 # number of professors
+c_num = 20 # number of courses
+e_num = 1000 #number of exam 
+ 
+# We will use pandas to display the queryset in tanular format
+import pandas
+pandas.options.display.max_rows=30
+
+# Utility function to display querysets
+def as_table(values_queryset):
+    return pandas.DataFrame(list(values_queryset))               
+
 #Filling Student table
-for i in range (0,200):
+for i in range (0,s_num):
     string_name = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
     string_family = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
     s =Student(first_name = string_name, family_name = string_family)
     s.save()
+    
+#Viewing the table
+#student_table = as_table(Student.objects.all().values("first_name", "family_name", "gpa","id")) 
+#display(student_table)  
+    
+    
 #Filling Professor table
-for i in range (0,10):
+for i in range (0,p_num):
     string_name = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
     string_family = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
     p= Professor(name = string_name+" "+ string_family)
     p.save()
 #Filling Course table
-##each professor have two courses.
-for i in range (1,21):
+## 
+prof_id_list=list(Professor.objects.all().values_list('id', flat=True))
+prof_id_seletion = random.choices(prof_id_list,k=c_num )
+
+
+#Filling Exam table
+for i in range (0, c_num):
     name = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
-    if (i>10):
-        c= Course(course_name=name, p_ID = i-10)
-    else:
-        c= Course(course_name=name, p_ID = i)
-        
+    c= Course(course_name=name, p_ID = prof_id_seletion[i])    
     c.save()
     
+student_id_list=list(Student.objects.all().values_list('id', flat=True))
+student_id_seletion = random.choices(student_id_list,k=e_num )
 
-    
-#Filling Exam
-#Each student can have up to max_num courses
-max_num=4
-s_num =  Student.objects.count()
-c_num =  Course.objects.count()
-course_num_list=[random.randint(1, max_num) for _ in range(s_num)]
-#e_num=sum(course_num_list)
-sid=1
-for i in range (0, s_num ):
-    a=course_num_list[i]
-    for j in range(0,a):
-        e=Exam (s_ID = sid, c_ID=random.randint(1,c_num), grade=round(random.uniform(0, 20), 2) )
-        e.save()
-        if j== a-1:
-            sid=sid+1
+course_id_list=list(Course.objects.all().values_list('id', flat=True))
+course_id_seletion = random.choices(course_id_list,k=e_num )
 
-#Calculating  GPA for each student 
-from django.db.models import Avg
-for i in range(1,s_num+1):
-    x = Exam.objects.filter (s_ID=i)
-    item=Student.objects.get(id=i)
+for i in range (0, e_num ):
+    e=Exam (s_ID = student_id_seletion[i], c_ID=course_id_seletion[i], grade=round(random.uniform(0, 20), 2) )
+    e.save()
+          
+            
+from django.db.models import Avg, F,Max, Window
+#from django.db.models.functions import RowNumber
+#exams=Exam.objects.annotate(row_number=Window(expression=RowNumber(), partition_by=[F('s_ID'), F('c_ID')], order_by=F('grade').desc(),),)
+exams=Exam.objects.annotate(max_grade=Window(expression=Max('grade'), partition_by=[F('s_ID'), F('c_ID')], order_by=F('grade').desc(),),)
+
+#Deleting dublicated records, Keep only the highest grade for each student's exam
+#borrowed from https://gist.github.com/victorono/cd9d14b013487b8b22975512225c5b4c
+unique_fields = ['s_ID', 'c_ID']
+duplicates = (
+    exams.all().values(*unique_fields)
+    .order_by()
+    .annotate(max_id=Max('id'), count_id=Count('id'))
+    .filter(count_id__gt=1)
+)
+
+for duplicate in duplicates:
+    (
+        exams.all()
+        .filter(**{x: duplicate[x] for x in unique_fields})
+        .exclude(id=duplicate['max_id'])
+        .delete()
+    )
+
+#Calculating the gpa for each student
+student_id_list = exams.all().values('s_ID').distinct() # getting the list of the available students IDs in exams queryset
+for item in student_id_list:
+    sid=item['s_ID']
+    x = exams.all().filter (s_ID=sid)
+    item=Student.objects.get(id=sid)
     avg=x.all().aggregate(Avg('grade'))
     item.gpa=round(avg['grade__avg'],2)
     item.save()
 
+
 for x in Student.objects.all():
-    print(x.first_name,' ',x.family_name,' ', x.gpa )
+    print(x.first_name,' ',x.family_name,' ', x.gpa, x.id )
     
-    
+#student_table = as_table(Student.objects.all().values("first_name", "family_name", "gpa","id")) 
+#display(student_table)     
+
 #Calculating pass percent for each professor
 #Eech professor has two courses. Pass_percent is calculated based on two courses 
 
-for p in Professor.objects.all():
-    p_courses= Course.objects.filter (p_ID=p.id)#return queryset
-    total =0
-    student_paased=0
-    for c in p_courses.all():
-        total = total + Exam.objects.filter (c_ID=c.id).all().count()
-        student_paased = student_paased + Exam.objects.filter (Q(c_ID=c.id) & Q(grade__gte=10)).all().count()
-    p.pass_percent=round(student_paased/total*100,2)
-    p.save()
-    
-for p in Professor.objects.all():
-    print(p.name, " ",p.pass_percent )
-    
-for c in Course.objects.all():
-    print(c.course_name," ", c.p_ID)
 
-for e in Exam.objects.all():
-    print(e.s_ID," ", e.c_ID, " " ,e.grade)
-  
