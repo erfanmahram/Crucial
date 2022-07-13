156de2e519e8d44d7a6831e138483b4cd1e82e3f
Erfan Mahram <erfanmahram@gmail.com>
2022-07-13 18:13:19 +0430

    migrate_db added

--- db_config.py
+++ db_config.py
@@ -1,1 +1,18 @@
-connection_string = 'sqlite:///crucial_db.sqlite'
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
+PYTHON_ENV = os.getenv('PYTHON_ENV')
+
+dev_connection_string = 'sqlite:///crucial_db.sqlite'
+prod_connection_string = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+if PYTHON_ENV == 'prod':
+    connection_string = prod_connection_string
+else:
+    connection_string = dev_connection_string
