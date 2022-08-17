55db94302ceebc02dba12b7007ab541c6a57d8f7
Amin Niazi <niazi@rahkar.co>
2022-08-17 16:25:23 +0430

    These features added to web: - recommender search - table of result - Suggestion Products page - tables of categories

--- web/Backend/api.py
+++ web/Backend/api.py
@@ -1,5 +1,5 @@
 try:
-    from flask import app, Flask, Response, request
+    from flask import app, Flask, Response, request, jsonify
     from flask_restful import Resource, Api, reqparse
     import elasticsearch
     from elasticsearch import Elasticsearch
@@ -8,11 +8,19 @@ try:
     import requests
     import json
     import es_config
+    from logzero import logger
+    from flask import Flask
+    from flask_sqlalchemy import SQLAlchemy
+    import db_config
+    from models import Resource, Model, Category, Brand, PageStatus
+    import os
 except Exception as e:
     print("Modules Missing {}".format(e))
 
 app = Flask(__name__)
 api = Api(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = db_config.connection_string
+db = SQLAlchemy(app)
 
 # ------------------------------------------------------------------------------------------------------------
 
@@ -20,36 +28,45 @@ NODE_NAME = 'crucial'
 es = Elasticsearch(hosts=es_config.elastic_connection_string, verify_certs=False,
                    ssl_show_warn=False)
 
+
 # ------------------------------------------------------------------------------------------------------------
 
 
-"""
-{
-"wildcard": {
-    "title": {
-        "value": "{}*".format(self.query)
-    }
-}
-}
-
-"""
-
-
-class Controller(Resource):
-    def __init__(self):
-        self.query = parser.parse_args().get("query", None)
-        self.baseQuery = {
+def get_query(name, brandName):
+    if brandName is None or len(brandName.strip()) == 0:
+        baseQuery = {
             "query": {
                 "match": {
-                    "brand_name": {
-                        "query": "{}".format(self.query)
-                    }
+                    "name": "{}*".format(name)
+                }
+            }
+        }
+    else:
+        baseQuery = {
+            "query": {
+                "bool": {
+                    "must": [
+                        {
+                            "match": {
+                                "brand_name": {
+                                    "query": "{}*".format(brandName)
+                                }
+                            }
+                        },
+                        {
+                            "match": {
+                                "name": {
+                                    "query": "{}*".format(name)
+                                }
+                            }
+                        }
+                    ]
                 }
             },
             "aggs": {
                 "auto_complete": {
                     "terms": {
-                        "field": "title.keyword",
+                        "field": "id",
                         "order": {
                             "_count": "desc"
                         },
@@ -58,10 +75,7 @@ class Controller(Resource):
                 }
             }
         }
-
-    def get(self):
-        res = es.search(index=NODE_NAME, size=0, body=self.baseQuery)
-        return res
+    return baseQuery
 
 
 @app.route('/brand-name', methods=['GET'])
@@ -87,61 +101,81 @@ def brand_name():
             }
         }
     }
-    res = es.search(index=NODE_NAME, size=10000, body=baseQuery)
+    res = es.search(index=NODE_NAME, size=15, body=baseQuery)
     result = list()
-    for index, item in enumerate(res['hits']['hits']):
-        result.append(dict(doc_count=index, key=item['_source']['brand_name']))
-    return dict(result=result)
+    res2 = list()
+    for i in res['hits']['hits']:
+        if i['_source']['brand_name'] not in result:
+            result.append(i['_source']['brand_name'])
+    for index, item in enumerate(result):
+        res2.append(dict(doc_count=index, key=item))
+    return dict(result=res2)
 
 
 @app.route('/name', methods=['GET'])
 def name():
-    query = request.args.get('query', None)
-    brandName = brand_name()['result']
-    baseQuery = {
-        "query": {
-            "bool": {
-                "must": [
-                    {
-                        "match": {
-                            "brand_name": {
-                                "query":  "{}*".format(brandName)
-                            }
-                        }
-                    },
-                    {
-                        "match": {
-                            "name": {
-                                "query": "{}*".format(query)
-                            }
-                        }
-                    }
-                ]
-            }
-        },
-        "aggs": {
-            "auto_complete": {
-                "terms": {
-                    "field": "id",
-                    "order": {
-                        "_count": "desc"
-                    },
-                    "size": 25
-                }
-            }
-        }
-    }
-    res = es.search(index=NODE_NAME, size=10000, body=baseQuery)
+    name = request.args.get('name', None)
+    brandName = request.args.get('brand', None)
+    baseQuery = get_query(name, brandName)
+    res = es.search(index=NODE_NAME, size=15, body=baseQuery)
     result = list()
     for index, item in enumerate(res['hits']['hits']):
         result.append(dict(doc_count=index, key=item['_source']['name']))
     return dict(result=result)
 
 
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    name = request.args.get('name', None)
+    brandName = request.args.get('brand', None)
+    baseQuery = get_query(name, brandName)
+    res = es.search(index=NODE_NAME, size=15, body=baseQuery)
+    result = list()
+    for index, item in enumerate(res['hits']['hits']):
+        result.append(dict(doc_count=index, key=item['_source']['id']))
+    json_result = list()
+    model_result = db.session.query(Model).filter(Model.Id.in_([i['key'] for i in result])).all()
+    for model in model_result:
+        category = db.session.query(Category).filter(Category.Id == model.CategoryId).first()
+        brand = db.session.query(Brand).filter(Brand.Id == category.BrandId).first()
+        json_result.append(
+            dict(ModelName=model.ModelName, MaximumMemory=model.MaximumMemory, Slots=model.Slots,
+                 StandardMemory=model.StandardMemory, StrgType=model.StrgType, CategoryName=category.CategoryName,
+                 BrandName=brand.BrandName, ModelUrl=model.ModelUrl, MoreInfo=model.SuggestInfo))
+    print(json_result)
+    j = str(json_result)
+    return j
+
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+my_files = r'/static/data/'
+file_dir = project_dir + my_files
+
+
+@app.route('/get_data', methods=['POST'])
+def get_data_function():
+    user = request.form['user']
+
+    if user == 'two':
+
+        json_file = file_dir + r'data_set_2.json'
+
+        with open(json_file) as f:
+            js_object = json.load(f)
+            return jsonify(js_object)
+
+    else:
+        json_file = file_dir + r'data_set_1.json'
+
+        with open(json_file) as f:
+            js_object = json.load(f)
+            return jsonify(js_object)
+
+
 parser = reqparse.RequestParser()
 parser.add_argument("query", type=str, required=True, help="query parameter is Required ")
 
-api.add_resource(Controller, "/autocomplete")
+# api.add_resource(Controller, "/autocomplete")
 
 if __name__ == '__main__':
     app.run(debug=True, port=4000)
--- web/Frontend/app.py
+++ web/Frontend/app.py
@@ -1,15 +1,44 @@
 from flask import Flask
-from flask import request, redirect, render_template, session
+from flask import request, render_template
+
 import requests
-import base64
-import requests
+import os
+import pandas as pd
+import json
 
 app = Flask(__name__)
+project_dir = os.path.dirname(os.path.abspath(__file__))
 
 
 @app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template("home.html")
+    # json_file = file_dir + r'data_set_1.json'
+    # with open(json_file) as f:
+    #     js_object = json.load(f)
+    #     df = pd.read_json(json.dumps(js_object))
+    return render_template('home.html')
+
+
+@app.route('/products', methods=["GET", "POST"])
+def products():
+    return render_template('products.html')
+
+
+@app.route('/table', methods=["GET", "POST"])
+def update_table():
+    name = request.form.get("data")
+    brand = request.form.get("brand")
+    params = {'name': name, 'brand': brand}
+    payload = {}
+    headers = {}
+    url = "http://127.0.0.1:4000/search?query="
+    response = requests.request("GET", url, headers=headers, data=payload, params=params)
+    my_json = response.content.decode('utf8').replace("'", '"').replace('None', '"?"').replace('""', '"').replace(', "],', '],')
+
+    data = json.loads(my_json)
+    json_file = json.dumps(data, sort_keys=False)
+    print(json_file)
+    return json_file
 
 
 @app.route('/pipe', methods=["GET", "POST"])
@@ -24,11 +53,13 @@ def pipe():
 
 @app.route('/pipes', methods=["GET", "POST"])
 def pipes():
-    data = request.form.get("data")
+    name = request.form.get("data")
+    brand = request.form.get("brand")
+    params = {'name': name, 'brand': brand}
     payload = {}
     headers = {}
-    url = "http://127.0.0.1:4000/name?query=" + str(data)
-    response = requests.request("GET", url, headers=headers, data=payload)
+    url = "http://127.0.0.1:4000/name?query="
+    response = requests.request("GET", url, headers=headers, data=payload, params=params)
     return response.json()
 
 
