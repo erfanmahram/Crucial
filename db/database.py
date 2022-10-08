fd299f475c619adbed8e9b61f7b8021b13aa4e38
Najme <51949100+CodeTild@users.noreply.github.com>
2022-10-08 19:24:34 +0330

    Update database.py
    Version2: Async features & Responses

--- db/database.py
+++ db/database.py
@@ -1,10 +1,7 @@
-from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-
-#from core.config import db_settings
 from core.configs import connection_string
-
-engine = create_engine(connection_string)
-SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
+engine = create_async_engine(connection_string, future=True, echo=True)
+async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 Base = declarative_base()
